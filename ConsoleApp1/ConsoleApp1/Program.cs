e7c3646f64546c8a535393100425355db7ec1e49
Mark Rybnikov <mark.rybnikov@amdaris.com>
2022-02-08 18:31:35 +0200

    Add some logic

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleApp1;
+
+    class Program
+{
+    static void main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Adding some logic");
+    }
+}
+
